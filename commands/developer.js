42ef97aa932d2a0cb8df0d1b852491a4c0604410
ToeBeanKing <72768118+ToeBeanKing@users.noreply.github.com>
2021-08-04 13:07:40 -0500

    Update developer.js

--- commands/developer.js
+++ commands/developer.js
@@ -2,6 +2,6 @@ module.exports= {
     name:'developer',
     description: "gives developer information",
     execute(message, args){
-        message.channel.send("My developer is ToeBeanKing/SegaSaturns!\nHe can be found on Twitter (@SegaSaturns), Tumblr (ToeBeanKing), and Instagram (@Sega.Saturns)!\nHe's the dev for AmityBot too!");
+        message.channel.send("My developer is blightguts!\nFae can be found on Twitter (@blightguts), Tumblr (blightguts), and Instagram (@blightguts)!\nFae is the dev for AmityBot too!");
     }
 }
